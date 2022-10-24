b0e2d8282e25ed4f4c00518a4850b69ca62ce48a
DESKTOP-BJAL2D9\jsekulska <joanna.sekulska-nalewajko@p.lodz.pl>
2022-10-24 10:43:59 +0200

    01 zmiana metody Policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+int Policz(int a, aint b) {
+    return a+b;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std:count << "W pierwszym branchu 2+3 = " << Policz(2,3) << std::endl;
     return 0;
 }
